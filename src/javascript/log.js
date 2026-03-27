977e4abb2c5e1b773ee528631642d19ea6cd4db9
DeveloperGY <dev.garrettyenojr@gmail.com>
2026-03-27 12:55:05 -0400

    Implemented log levels

--- src/javascript/log.js
+++ src/javascript/log.js
@@ -1,16 +1,39 @@
+var LogLevel;
+(function (LogLevel) {
+    LogLevel[LogLevel["Error"] = 0] = "Error";
+    LogLevel[LogLevel["Warn"] = 1] = "Warn";
+    LogLevel[LogLevel["Info"] = 2] = "Info";
+    LogLevel[LogLevel["Debug"] = 3] = "Debug";
+    LogLevel[LogLevel["Trace"] = 4] = "Trace";
+})(LogLevel || (LogLevel = {}));
 class Logger {
-    constructor() { }
+    constructor(log_level) {
+        this.log_level = log_level;
+    }
+    trace(system, message) {
+        if (this.log_level >= LogLevel.Trace) {
+            console.log(`[TRACE]: ${system} - ${message}`);
+        }
+    }
     debug(system, message) {
-        console.debug(`[DEBUG]: ${system} - ${message}`);
+        if (this.log_level >= LogLevel.Debug) {
+            console.debug(`[DEBUG]: ${system} - ${message}`);
+        }
     }
     info(system, message) {
-        console.info(`[INFO]: ${system} - ${message}`);
+        if (this.log_level >= LogLevel.Info) {
+            console.info(`[INFO]: ${system} - ${message}`);
+        }
     }
     warn(system, message) {
-        console.warn(`[WARN]: ${system} - ${message}`);
+        if (this.log_level >= LogLevel.Warn) {
+            console.warn(`[WARN]: ${system} - ${message}`);
+        }
     }
     err(system, message) {
-        console.error(`[ERROR]: ${system} - ${message}`);
+        if (this.log_level >= LogLevel.Error) {
+            console.error(`[ERROR]: ${system} - ${message}`);
+        }
     }
 }
-export const LOGGER = new Logger();
+export const LOGGER = new Logger(LogLevel.Trace);
--- src/typescript/log.ts
+++ src/typescript/log.ts
@@ -1,21 +1,47 @@
+enum LogLevel {
+  Error = 0,
+  Warn = 1,
+  Info = 2,
+  Debug = 3,
+  Trace = 4,
+}
+
 class Logger {
-  constructor() {}
+  private log_level: LogLevel;
+
+  constructor(log_level: LogLevel) {
+    this.log_level = log_level;
+  }
+
+  trace(system: string, message: string) {
+    if (this.log_level >= LogLevel.Trace) {
+      console.log(`[TRACE]: ${system} - ${message}`);
+    }
+  }
 
   debug(system: string, message: string) {
-    console.debug(`[DEBUG]: ${system} - ${message}`);
+    if (this.log_level >= LogLevel.Debug) {
+      console.debug(`[DEBUG]: ${system} - ${message}`);
+    }
   }
 
   info(system: string, message: string) {
-    console.info(`[INFO]: ${system} - ${message}`);
+    if (this.log_level >= LogLevel.Info) {
+      console.info(`[INFO]: ${system} - ${message}`);
+    }
   }
 
   warn(system: string, message: string) {
-    console.warn(`[WARN]: ${system} - ${message}`);
+    if (this.log_level >= LogLevel.Warn) {
+      console.warn(`[WARN]: ${system} - ${message}`);
+    }
   }
 
   err(system: string, message: string) {
-    console.error(`[ERROR]: ${system} - ${message}`);
+    if (this.log_level >= LogLevel.Error) {
+      console.error(`[ERROR]: ${system} - ${message}`);
+    }
   }
 }
 
-export const LOGGER = new Logger();
+export const LOGGER = new Logger(LogLevel.Trace);
